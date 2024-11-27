ec838455851027bd5d04759464ee1ced1c2a4f68
beaflowers <157410173+beaflowers@users.noreply.github.com>
2024-11-26 22:06:02 -0500

    Update code.py
    added touch.io sensor

--- Circuit_Python/code.py
+++ Circuit_Python/code.py
@@ -5,13 +5,9 @@ import touchio
 import busio
 import time
 import adafruit_mpr121
-# patch-1
-from adafruit_mpr121 import MPR121
 
-
-
-#main
 #initializing pins
+
 #debug LED
 default_led = digitalio.DigitalInOut(board.LED)
 default_led.direction = digitalio.Direction.OUTPUT
@@ -19,29 +15,6 @@ default_led.direction = digitalio.Direction.OUTPUT
 #myoware sensor pins
 human_myoware = analogio.AnalogIn(board.GP26)
 plant_myoware = analogio.AnalogIn(board.GP28)
-#patch-1
-
-#photoresistor
-photoresistor = analogio.AnalogIn(board.GP27)
-
-
-while True:
-    default_led.value = True
-
-    #myoware sensor
-    #0-65535 across voltage range 0.0v - 3.3v
-    hu_reading = human_myoware.value #Read sensor data
-    hu_voltage = (hu_reading / 65535) * 3.3  # This gives the value in volts
-    print(f"Human Voltage: {hu_voltage:.2f} V")
-
-    pla_reading = plant_myoware.value
-    pla_voltage = (pla_reading / 65535) * 3.3
-    print(f"Plant Voltage: {pla_voltage:.2f} V")
-
-    #photoresistor sensor values, goes UP when light is blocked
-    light_value = photoresistor.value
-    print(f"Light Value: {light_value:.2f}")
-=======
 
 #photoresistor
 photoresistor = analogio.AnalogIn(board.GP27)
@@ -50,10 +23,17 @@ photoresistor = analogio.AnalogIn(board.GP27)
 i2c = busio.I2C(scl=board.GP7, sda=board.GP6)
 touch_pad = adafruit_mpr121.MPR121(i2c)
 
+#capacitive plant
+touch_pin = touchio.TouchIn(board.GP15)
+
 def touched():
     for i in range(12):
         if touch_pad[i].value:
-            print(f"Touched pad # {i}!")
+            print(i)
+
+def plant_touch():
+    if touch_pin.value:
+        print(1)
 
 
 def myoware():
@@ -81,6 +61,7 @@ while True:
     photo_sensor()
 
     touched()
-# main
+
+    plant_touch()
 
     time.sleep(0.5) #small delay so the serial terminal is not flooded with data
